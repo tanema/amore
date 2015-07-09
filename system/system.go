6f84a68ea9d25453c73b33ba9d5971b815b40627
Tim Anema <timanema@gmail.com>
2015-07-08 21:06:30 -0400

    implemented some of the system stuf

--- system/system.go
+++ system/system.go
@@ -1,19 +1,34 @@
 package system
 
-func GetClipboardText() {
+import (
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.1/glfw"
+	"github.com/skratchdot/open-golang/open"
+
+	"github.com/tanema/amore/window"
+)
+
+func GetClipboardText() (string, error) {
+	return window.GetCurrent().GetClipboardString()
 }
 
-func GetOS() {
+func SetClipboardText(str string) {
+	window.GetCurrent().GetClipboardString(str)
+}
+
+func GetOS() string {
+	return runtime.GOOS
 }
 
 func GetPowerInfo() {
+
 }
 
-func GetProcessorCount() {
+func GetProcessorCount() int {
+	return runtime.NumCPU()
 }
 
-func OpenUrl() {
-}
-
-func SetClipboardText() {
+func OpenUrl(url string) {
+	open.Run(url)
 }
