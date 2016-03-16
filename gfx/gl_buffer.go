b800a654296447b7faa5784dd2f61ac05d6ec2a5
Tim Anema <timanema@gmail.com>
2016-03-16 19:02:31 -0400

    fixed index buffer again because the sprite batch draw range was not working correctly

--- gfx/gl_buffer.go
+++ gfx/gl_buffer.go
@@ -159,7 +159,7 @@ func (buffer *indexBuffer) fill(offset int, data []uint32) {
 func (buffer *indexBuffer) drawElements(mode uint32, offset, size int) {
 	buffer.bind()
 	defer buffer.unbind()
-	gl.DrawElements(gl.Enum(mode), size, gl.UNSIGNED_INT, offset)
+	gl.DrawElements(gl.Enum(mode), size, gl.UNSIGNED_INT, offset*4)
 }
 
 func (buffer *indexBuffer) getData() []uint32 {
